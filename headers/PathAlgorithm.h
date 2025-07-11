c8b01dca727ad6f9afbbaa95dbb3c48d1f8815c8
Aadim <128502526+aadimsapkota134@users.noreply.github.com>
2025-07-11 12:08:02 +0545

    Update PathAlgorithm.h for Dijkstra/A* updation on PQ

--- headers/PathAlgorithm.h
+++ headers/PathAlgorithm.h
@@ -6,6 +6,25 @@
 #include <QtConcurrent>
 #include <QFuture>
 #include "GridView.h"
+#include <queue> // for std::priority_queue
+#include <cmath> // For fabsf
+
+// For Dijkstra: orders by localGoal (min-heap)
+struct CompareNodesDijkstra {
+    bool operator()(Node* a, Node* b) {
+        // For a min-heap, return true if 'a' has lower priority than 'b' (i.e., a's goal is greater than b's)
+        return a->localGoal > b->localGoal;
+    }
+};
+
+// For AStar: orders by globalGoal (min-heap)
+struct CompareNodesAStar {
+    bool operator()(Node* a, Node* b) {
+        // For a min-heap, return true if 'a' has lower priority than 'b' (i.e., a's goal is greater than b's)
+        return a->globalGoal > b->globalGoal;
+    }
+};
+
 
 class PathAlgorithm : public QObject
 {
